9e99b5bba2216756711e3a02d6eb6c0b94fe8e38
xlinx@decade <xlinx@decade.tw>
2024-09-03 16:56:37 +0800

    install lib

--- install.py
+++ install.py
@@ -23,7 +23,7 @@ PyOpenGL_accelerate
 import launch
 
 if not launch.is_installed("PyOpenGL"):
-    launch.run_pip(f"install PyOpenGL PyOpenGL_accelerate", "PyOpenGL PyOpenGL_accelerate")
+    launch.run_pip(f"install SpoutGL PyOpenGL PyOpenGL_accelerate", "SpoutGL PyOpenGL PyOpenGL_accelerate")
 # if not launch.is_installed("OpenGL_accelerate"):
 #     launch.run_pip(f"install -U OpenGL_accelerate", "OpenGL_accelerate")
 # if not launch.is_installed("numpy"):
--- scripts/SpoutGL/helpers.py
+++
@@ -1,1 +0,0 @@
-from ._spoutgl.helpers import *
